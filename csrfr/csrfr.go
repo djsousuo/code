89f36761cf1df87ac02e6819582942f59eade8d6
movsx <movsx@icloud.com>
2020-04-01 08:49:45 -0500

    updates

--- csrfr/csrfr.go
+++ csrfr/csrfr.go
@@ -59,15 +59,17 @@ func (header *headerFlags) Set(str string) error {
  * primary HTTP handler
  *
  */
-func checkHost(target Targets, payload Payloads, client *http.Client) error {
+func checkHost(target Targets, payload Payloads, client *http.Client) (bool, error) {
 	positive := false
 	req, err := http.NewRequest(target.Method, target.URL, strings.NewReader(target.Params))
 	if err != nil {
-		return err
+		return false, err
 	}
 
+        req.Header.Set("User-Agent", Options.UA)
 	if len(Options.Headers) > 0 {
 	}
+        //fmt.Printf("URL: %s\n", target.URL)
 
 	switch payload.Attack {
 	/*
@@ -76,10 +78,10 @@ func checkHost(target Targets, payload Payloads, client *http.Client) error {
 	case "cors":
 		req.Header.Set("Origin", Options.Origin)
 		resp, err := client.Do(req)
-		defer resp.Body.Close()
 		if err != nil {
-			return err
+			return false, err
 		}
+		defer resp.Body.Close()
 		if resp != nil {
 			origin := resp.Header.Get("Access-Control-Allow-Origin")
 			if origin == Options.Origin || origin == "*" || origin == string("*."+Options.Origin) {
@@ -93,13 +95,13 @@ func checkHost(target Targets, payload Payloads, client *http.Client) error {
 	 */
 	case "csrf":
 		if target.Method != "POST" {
-			return errors.New("Method not POST on CSRF")
+			return false, errors.New("Method not POST on CSRF")
 		}
 		resp, err := client.Do(req)
-		defer resp.Body.Close()
 		if err != nil {
-			return err
+			return false, err
 		}
+		defer resp.Body.Close()
 
 		if resp != nil && resp.StatusCode == 200 {
 			positive = true
@@ -107,13 +109,18 @@ func checkHost(target Targets, payload Payloads, client *http.Client) error {
 	/*
 	 * positive: response delay >= whats specified in the injection
 	 */
+        case "ssrf":
+		/* ideally we want some sort of server that spins up and detects these
+		 * for the mean time i'll have to rely on a long delay and just weed out the false positives
+		 */
+                fallthrough
 	case "sqli":
 		var start time.Time
 		trace := &httptrace.ClientTrace{
 			GotFirstResponseByte: func() {
-				fmt.Printf("Time since start: %v\n", time.Since(start))
-				// this isn't right clearly. placeholder
-				if time.Since(start) > 10000 {
+                                t := time.Since(start)
+				if t.Seconds()*10 >= 10 {
+                                        //fmt.Printf("[*] Delay - Payload: %s, URL: %s\n", payload.Payload, target.URL)
 					positive = true
 				}
 			},
@@ -121,40 +128,52 @@ func checkHost(target Targets, payload Payloads, client *http.Client) error {
 		req = req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
 		start = time.Now()
 		if _, err := http.DefaultTransport.RoundTrip(req); err != nil {
-			return err
+			return false, err
 		}
+                defer req.Body.Close()
+                fallthrough
+
 	/*
 	 * positive: payload reflected in response body
 	 */
 	case "xss":
 		resp, err := client.Do(req)
-		defer resp.Body.Close()
-		if err != nil {
-			return err
+		if err != nil || resp.StatusCode != 200 {
+			return false, err
 		}
+		defer resp.Body.Close()
 
 		bodyBytes, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
-			return err
+			return false, err
 		}
 		bodyStr := string(bodyBytes)
 
-		if strings.Contains(bodyStr, payload.Payload) {
+		if strings.Contains(bodyStr, "XXSTRINGXX") {
 			positive = true
 		}
-	case "ssrf":
-		/* ideally we want some sort of server that spins up and detects these
-		 * for the mean time i'll have to rely on a long delay and just weed out the false positives
-		 */
 	case "lfi":
+                fallthrough
 	case "cmd":
+                resp, err := client.Do(req)
+                if err != nil || resp.StatusCode != 200 {
+                        return false, err
+                }
+                defer resp.Body.Close()
+
+                bodyBytes, err := ioutil.ReadAll(resp.Body)
+                if err != nil {
+                        return false, err
+                }
+
+                bodyStr := string(bodyBytes)
+                if strings.Contains(bodyStr, "root:x:") {
+                        positive = true
+                }
+
 	}
 
-	if positive == true {
-		fmt.Printf("[+] VULNERABLE: (%s - %s) %s %s - Parameters: %s\n", payload.Attack, payload.Payload, target.Method, target.URL, target.Params)
-	}
-
-	return nil
+	return positive, nil
 }
 
 /*
@@ -196,7 +215,7 @@ func generateFuzz(target Targets, payload Payloads) ([]Targets, error) {
 				}
 				switch target.Method {
 				case "GET":
-					t = append(t, Targets{target.Method, string(strings.Split(target.URL, "?")[0] + q.Encode()), ""})
+					t = append(t, Targets{target.Method, string(strings.Split(target.URL, "?")[0] + "?" + q.Encode()), ""})
 				case "POST":
 					t = append(t, Targets{target.Method, target.URL, q.Encode()})
 				}
@@ -208,25 +227,28 @@ func generateFuzz(target Targets, payload Payloads) ([]Targets, error) {
 
 func main() {
 	var err error
-	var targets []Targets
+	var endpoint []Targets
 	var payloads []Payloads
-	hostInput := os.Stdin
+        var inputFile string
+	type job struct {
+		t Targets
+		p Payloads
+	}
 
 	flag.IntVar(&Options.Concurrency, "c", 20, "Number of concurrent requests")
-	flag.IntVar(&Options.Throttle, "d", 2, "Delay between requests (seconds)")
+	flag.IntVar(&Options.Throttle, "d", 2, "Delay between requests (milliseconds)")
 	flag.BoolVar(&Options.Verbose, "v", false, "Verbose mode (default: false)")
 	flag.StringVar(&Options.Origin, "o", "evil.com", "Origin")
+        flag.StringVar(&inputFile, "input", "ez.json", "Input file")
 	flag.StringVar(&Options.UA, "ua", "Mozilla/5.0 (Windows Phone 10.0; Android 6.0.1; Microsoft; RM-1152) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Mobile Safari/537.36 Edge/15.15254", "User Agent")
 	flag.Var(&Options.Headers, "h", "Add custom header. Can be specified multiple times")
 	flag.Var(&Options.Cookies, "cookie", "Add cookies, seperated by ;")
 	flag.IntVar(&Options.Timeout, "timeout", 30, "HTTP timeout (seconds)")
 	flag.Parse()
 
-	if len(os.Args) > 1 {
-		hostInput, err = os.Open(os.Args[1])
-		if err != nil {
-			log.Fatal(err)
-		}
+        hostInput, err := os.Open(inputFile)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	var client = &http.Client{
@@ -241,7 +263,7 @@ func main() {
 		},
 	}
 
-	queue := make(chan Targets)
+	queue := make(chan job)
 	var wg sync.WaitGroup
 
 	for i := 0; i < Options.Concurrency; i++ {
@@ -249,37 +271,20 @@ func main() {
 		go func() {
 			defer wg.Done()
 			for target := range queue {
-				for _, special := range []string{"csrf", "cors"} {
-					/* these don't require permutation and will be sent as-is */
-					err := checkHost(target, Payloads{special, ""}, client)
-					if err != nil && Options.Verbose {
-						fmt.Println("[-] Error: ", err)
-					}
-					time.Sleep(time.Duration(Options.Throttle) * time.Millisecond)
-				}
+				result, err := checkHost(target.t, target.p, client)
+				if err != nil && Options.Verbose {
+					fmt.Println("[-] Error: ", err)
+				} else if err == nil && result == true {
+                                        switch target.p.Attack {
+                                                case "cors":
+                                                        fallthrough
+                                                case "csrf":
+                                                        fmt.Printf("[+] VULNERABLE: (%s) %s %s\n", strings.ToUpper(target.p.Attack), target.t.Method, target.t.URL)
+                                                default:
+		                                        fmt.Printf("[+] VULNERABLE: (%s) %s %s - Parameters: %s\n", strings.ToUpper(target.p.Attack), target.t.Method, target.t.URL, target.t.Params)
+                                                }
+                                }
 
-				for x := range payloads {
-					/*
-					 * need to account for 20 (or however many) go routines with X amount of payloads, with multiple variations for each one
-					 * throttling will need to be added as well as better go routine management so each one isn't caught up with 500 payloads per URL
-					 *
-					 * it will end up being a total of n[params]*2 per payload
-					 * with each param being: unmodified, appended with fuzz string, and replaced with fuzz string
-					 */
-					var currentTarget []Targets
-					currentTarget, err = generateFuzz(target, payloads[x])
-					if err != nil && Options.Verbose {
-						fmt.Println("[-] Error: ", err)
-					}
-
-					for y := range currentTarget {
-						err := checkHost(currentTarget[y], payloads[x], client)
-						if err != nil && Options.Verbose {
-							fmt.Println("[-] Error: ", err)
-						}
-						time.Sleep(time.Duration(Options.Throttle) * time.Millisecond)
-					}
-				}
 			}
 		}()
 	}
@@ -290,7 +295,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	err = json.Unmarshal(fp, &targets)
+	err = json.Unmarshal(fp, &endpoint)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -306,8 +311,32 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for n := range targets {
-		queue <- targets[n]
+	for n := range endpoint {
+                baseURL := strings.Split(endpoint[n].URL, "?")[0]
+                if len(baseURL) > 0 && Options.Verbose {
+                        fmt.Println("[*] Fuzzing:", baseURL)
+                }
+		for _, special := range []string{"csrf", "cors"} {
+			/* these don't require permutation and will be sent as-is */
+                        queue <- job{endpoint[n], Payloads{special, ""}}
+			time.Sleep(time.Duration(Options.Throttle) * time.Millisecond)
+		}
+
+		for x := range payloads {
+			/*
+			 * it will end up being a total of params*2 per payload
+			 * with each param being: unmodified, appended with fuzz string, and replaced with fuzz string
+			 */
+                        currentEndpoint, err := generateFuzz(endpoint[n], payloads[x])
+			if err != nil && Options.Verbose {
+				fmt.Println("[-] Error: ", err)
+			}
+
+			for y := range currentEndpoint {
+                                queue <- job{currentEndpoint[y], payloads[x]}
+				time.Sleep(time.Duration(Options.Throttle) * time.Millisecond)
+			}
+		}
 	}
 	close(queue)
 	wg.Wait()
