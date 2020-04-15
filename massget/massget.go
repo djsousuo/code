08fdd2d8e124e799f8d2cb581bc8684d13b532d7
movsx <movsx@icloud.com>
2020-04-15 09:42:54 -0500

    stuff

--- massget/massget.go
+++ massget/massget.go
@@ -5,7 +5,8 @@ import (
 	"crypto/tls"
 	"flag"
 	"fmt"
-	"log"
+        "net"
+        "time"
 	"net/http"
 	"os"
 	"strings"
@@ -23,13 +24,13 @@ func fetch(url string) {
 	client := http.DefaultClient
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatal(err)
+                return
 	}
 
         if response != 0 && resp.StatusCode == response {
                 fmt.Println(url)
         } else {
-	        fmt.Println(resp.StatusCode, url, strings.Join(resp.Header["Content-Type"], " "), resp.Header["Server"])
+	        fmt.Println(resp.StatusCode, url, resp.Header["Content-Type"], resp.Header["Server"])
         }
 	resp.Body.Close()
 }
@@ -37,13 +38,16 @@ func fetch(url string) {
 func main() {
 	var method string
 	var concurrency int
+        var timeout int
 
 	flag.StringVar(&method, "method", "GET", "HTTP Method to use (GET/POST)")
         flag.IntVar(&response, "r", 0, "Return only URL's matching HTTP response code (0 = all)")
 	flag.IntVar(&concurrency, "c", 20, "Number of concurrent requests (default: 20)")
+        flag.IntVar(&timeout, "t", 10, "Timeout (seconds)")
 	flag.Parse()
 
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+        http.DefaultTransport.(*http.Transport).DialContext = (&net.Dialer{Timeout: time.Duration(time.Duration(timeout) * time.Second), KeepAlive: time.Second,}).DialContext
 
 	urls := make(chan string)
 	var wg sync.WaitGroup
