be6e0eb253439c08ab4df8de48188db6f0473462
movsx <movsx@icloud.com>
2020-04-22 12:45:46 -0500

    add md5sum and redirect support

--- massget/massget.go
+++ massget/massget.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"bufio"
+	"crypto/md5"
 	"crypto/tls"
 	"flag"
 	"fmt"
-        "net"
-        "time"
+	"io/ioutil"
+	"net"
 	"net/http"
 	"os"
 	"strings"
 	"sync"
+	"time"
 )
 
 var response int
@@ -21,33 +23,49 @@ func fetch(url string) {
 		return
 	}
 
-	client := http.DefaultClient
-	resp, err := client.Do(req)
-	if err != nil {
-                return
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
 	}
 
-        if response != 0 && resp.StatusCode == response {
-                fmt.Println(url)
-        } else {
-	        fmt.Println(resp.StatusCode, url, resp.Header["Content-Type"], resp.Header["Server"])
-        }
+	resp, err := client.Do(req)
+	if err != nil {
+		return
+	}
+
+	if (response != 0 && resp.StatusCode == response) || response == 0 {
+		mime := "UNKNOWN"
+		server := "UNKNOWN"
+		if len(resp.Header["Content-Type"]) > 0 {
+			mime = strings.Split(resp.Header["Content-Type"][0], ";")[0]
+		}
+		if len(resp.Header["Server"]) > 0 {
+			server = resp.Header["Server"][0]
+		}
+		bodyBytes, _ := ioutil.ReadAll(resp.Body)
+		fmt.Printf("%d %s %s %s %x ", resp.StatusCode, url, mime, server, md5.Sum(bodyBytes))
+		if len(resp.Header["Location"]) > 0 {
+			fmt.Printf("--> %s", resp.Header["Location"][0])
+		}
+		fmt.Printf("\n")
+	}
 	resp.Body.Close()
 }
 
 func main() {
 	var method string
 	var concurrency int
-        var timeout int
+	var timeout int
 
 	flag.StringVar(&method, "method", "GET", "HTTP Method to use (GET/POST)")
-        flag.IntVar(&response, "r", 0, "Return only URL's matching HTTP response code (0 = all)")
+	flag.IntVar(&response, "r", 0, "Return only URL's matching HTTP response code (0 = all)")
 	flag.IntVar(&concurrency, "c", 20, "Number of concurrent requests (default: 20)")
-        flag.IntVar(&timeout, "t", 10, "Timeout (seconds)")
+	flag.IntVar(&timeout, "t", 10, "Timeout (seconds)")
 	flag.Parse()
 
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-        http.DefaultTransport.(*http.Transport).DialContext = (&net.Dialer{Timeout: time.Duration(time.Duration(timeout) * time.Second), KeepAlive: time.Second,}).DialContext
+	http.DefaultTransport.(*http.Transport).DialContext = (&net.Dialer{Timeout: time.Duration(timeout) * time.Second, KeepAlive: time.Second}).DialContext
 
 	urls := make(chan string)
 	var wg sync.WaitGroup
@@ -55,7 +73,7 @@ func main() {
 	for i := 0; i < concurrency; i++ {
 		wg.Add(1)
 		go func() {
-                        defer wg.Done()
+			defer wg.Done()
 			for u := range urls {
 				fetch(u)
 			}
