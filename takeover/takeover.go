f1fd86f5eda58744a05313c0c2297564d8cd99a4
movsx <dpt@wearehackerone.com>
2020-05-13 12:13:36 -0500

    improvements

--- takeover/takeover.go
+++ takeover/takeover.go
@@ -6,7 +6,6 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"fmt"
-	"github.com/miekg/dns"
 	"io/ioutil"
 	"log"
 	"net"
@@ -24,11 +23,6 @@ type Fingerprints struct {
 	Nxdomain    bool     `json:"nxdomain"`
 }
 
-type Message struct {
-	Username string `json:"username"`
-	Message  string `json:"content"`
-}
-
 type State struct {
 	sync.Mutex
 	Start   time.Time
@@ -43,20 +37,30 @@ var Config struct {
 		Webhook  string  `json:"webhook"`
 		Timeout  float64 `json:"timeout"`
 		MaxLen   int     `json:"maxentries"`
-	}
-	NS          string
-	UA          string
-	Timeout     int
-	Retries     int
-	Verbose     bool
-	Concurrency int
+	} `json:"discord"`
+	NS          string	`json:"nameserver"`
+	UA          string	`json:"user_agent"`
+	Timeout     int		`json:"timeout"`
+	Retries     int		`json:"retries"`
+	Concurrency int		`json:"concurrency"`
 }
 var fpItems []Fingerprints
 
 func discordMsg(msg string, state *State) error {
+	type message struct {
+		Username string `json:"username"`
+		Message  string `json:"content"`
+	}
+
+	if len(msg) < 1 {
+		return nil
+	}
+
 	format := "```" + msg + "```"
-	m := Message{Config.Discord.Username,
-		format}
+	m := message{
+		Username: Config.Discord.Username,
+		Message:  format,
+	}
 
 	data, err := json.Marshal(m)
 	if err != nil {
@@ -94,139 +98,73 @@ func discordTimer(state *State) {
 func absoluteHost(host string) string {
 	if strings.Contains(host, "://") {
 		return strings.Split(host, "://")[1] + "."
-	} else {
-		return host + "."
 	}
+
+	return host + "."
 }
 
-func dnsCNAME(host string) (string, error) {
-	var cname string
-	msg := new(dns.Msg)
-	msg.SetQuestion(host, dns.TypeA)
-	reply, err := dns.Exchange(msg, Config.NS)
-
-	if err != nil {
-		return "", err
-	}
-
-	for _, answer := range reply.Answer {
-		if t, ok := answer.(*dns.CNAME); ok {
-			cname = t.Target
-		}
-	}
-
-	return cname, nil
-}
-
-func dnsNS(host string) []string {
-	var ns []string
-	msg := new(dns.Msg)
-	msg.SetQuestion(host, dns.TypeNS)
-	reply, err := dns.Exchange(msg, Config.NS)
-	if err != nil {
-		return nil
-	}
-
-	for _, answer := range reply.Answer {
-		if t, ok := answer.(*dns.NS); ok {
-			ns = append(ns, t.Ns)
-		}
-	}
-
-	return ns
-}
-
-func dnsA(host string, ns string) (string, bool) {
-	msg := new(dns.Msg)
-	msg.SetQuestion(host, dns.TypeA)
-	reply, err := dns.Exchange(msg, ns+":53")
-	if err != nil {
-		return "", false
-	}
-
-	if reply.Rcode == dns.RcodeServerFailure || reply.Rcode == dns.RcodeRefused {
-		fmt.Println("[*] Found AWS NXDOMAIN with host: " + host + ", NS: " + ns)
-		return ns, true
-	}
-	return "", false
-}
-
-func dnsNX(host string) bool {
-	if _, err := net.LookupHost(host); err != nil {
-		if strings.Contains(err.Error(), "no such host") {
-			return true
-		}
-	}
-	return false
-}
-
-func matchAWS(host []string) {
-}
-
-func matchFingerprint(cname string) (bool, int) {
-	if cname == "" {
-		return false, -1
-	}
-
+func matchFingerprint(cname string) *Fingerprints {
 	for n := range fpItems {
 		for x := range fpItems[n].Cname {
 			if strings.Contains(cname, fpItems[n].Cname[x]) {
-				return true, n
+				return &fpItems[n]
 			}
 		}
 	}
 
-	return false, -1
+	return nil
 }
 
 func checkHost(host string, state *State) {
 	aHost := absoluteHost(host)
-	nx := dnsNX(strings.Split(host, "://")[1])
+	cname, _ := dnsCNAME(aHost)
+	if cname == "" {
+		return
+	}
 
-	cname, err := dnsCNAME(aHost)
+	found := matchFingerprint(cname)
+	if found != nil {
+		nx := false
+		if answer, err := dnsA(cname, Config.NS); answer == nil && err == nil {
+			nx = true
+		}
+
+		if (found.Nxdomain && nx) || (verifyFingerprint(host, found.Fingerprint, state) && !nx) {
+			str := fmt.Sprintf("[*] %s %s CNAME: %s", strings.ToUpper(found.Service), host, cname)
+			fmt.Println(str)
+			state.Update(str)
+		}
+	}
 
 	/* edge case: route53 with SERVFAIL/REFUSED at awsdns */
-	if nx && err == nil {
-		ns := dnsNS(aHost)
-		if ns != nil {
-			for i := range ns {
-				if strings.Contains(ns[i], "awsdns") {
-					if cname != "" {
-						dnsA(cname, ns[i])
-					} else {
-						dnsA(aHost, ns[i])
+	/*
+		if nx && err == nil {
+			ns := dnsNS(aHost)
+			if ns != nil {
+				for i := range ns {
+					if strings.Contains(ns[i], "awsdns") {
+						if cname != "" {
+							dnsA(cname, ns[i])
+						} else {
+							dnsA(aHost, ns[i])
+						}
 					}
 				}
 			}
 		}
-	}
-
-	found, index := matchFingerprint(cname)
-	if found {
-		/* NXDOMAIN takeover */
-		if fpItems[index].Nxdomain && nx {
-			str := fmt.Sprintf("[*] %s NXDOMAIN: %s CNAME: %s", strings.ToUpper(fpItems[index].Service), host, cname)
-			fmt.Println(str)
-			state.Update(str)
-			return
-		}
-
-		/* traditional CNAME takeover with website fingerprint */
-		if verifyFingerprint(host, cname, fpItems[index].Fingerprint, state) {
-			str := fmt.Sprintf("[*] %s %s CNAME: %s", strings.ToUpper(fpItems[index].Service), host, cname)
-			fmt.Println(str)
-			state.Update(str)
-			return
-		}
-	}
+	*/
 }
 
-func verifyFingerprint(host string, cname string, fp []string, state *State) bool {
+func verifyFingerprint(host string, fp []string, state *State) bool {
+	if !strings.HasPrefix(host, "http://") && !strings.HasPrefix(host, "https://") {
+		host = "https://" + host
+	}
+
 	req, err := http.NewRequest("GET", host, nil)
 	if err != nil {
 		return false
 	}
-	req.Header.Set("User-Agent", "User-Agent: Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19")
+	req.Header.Set("User-Agent", Config.UA)
 
 	resp, err := state.Client.Do(req)
 	if err != nil {
@@ -236,9 +174,9 @@ func verifyFingerprint(host string, cname string, fp []string, state *State) boo
 	if resp != nil {
 		bodyBytes, _ := ioutil.ReadAll(resp.Body)
 		resp.Body.Close()
-		bodyStr := string(bodyBytes)
+		bodyStr := strings.ToLower(string(bodyBytes))
 		for i := range fp {
-			if strings.Contains(bodyStr, fp[i]) {
+			if strings.Contains(bodyStr, strings.ToLower(fp[i])) {
 				return true
 			}
 		}
@@ -285,7 +223,10 @@ func main() {
 		}
 	}
 
-	timeout := 15 * time.Second
+	loadJSON("fingerprints.json", &fpItems)
+	loadJSON("config.json", &Config)
+
+	timeout := time.Duration(time.Duration(Config.Timeout) * time.Second)
 	var transport = &http.Transport{
 		MaxIdleConns:      30,
 		IdleConnTimeout:   time.Second,
@@ -307,18 +248,17 @@ func main() {
 		},
 	}
 
-	loadJSON("config.json", &fpItems)
-	loadJSON("bot.json", &Config.Discord)
-	Config.NS = "4.2.2.4:53"
 	hosts := make(chan string)
 	var wg sync.WaitGroup
 
-	for i := 0; i < 30; i++ {
+	for i := 0; i < Config.Concurrency; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
 			for host := range hosts {
-				//discordTimer(state)
+				if Config.Discord.Webhook != "" {
+					discordTimer(state)
+				}
 				checkHost(host, state)
 			}
 		}()
@@ -326,12 +266,7 @@ func main() {
 
 	scanner := bufio.NewScanner(hostInput)
 	for scanner.Scan() {
-		current := scanner.Text()
-		if !strings.HasPrefix(current, "http://") && !strings.HasPrefix(current, "https://") {
-			current = "https://" + current
-		}
-
-		hosts <- current
+		hosts <- scanner.Text()
 	}
 
 	close(hosts)
